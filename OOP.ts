7970a622e3b0d633cf977a8adce872a3a611a324
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-09-25 11:28:48 -0600

    First commit

---
+++ OOP.ts
@@ -0,0 +1,13 @@
+export class Message {
+  title;
+  message;
+  id;
+}
+
+function Message2(title, message, id) {
+  this.title = title;
+  this.message = message;
+  this.id = id;
+}
+
+const message2 = new Message2("hello", "world", "infinity");
